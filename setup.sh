934f0cfa23903b3acbebf35f71b4e661ab0a7c3e
everythinguwannatobe <adonishartsuper@gmail.com>
2025-02-28 16:18:48 +0300

    change requirements.txt

--- setup.sh
+++ setup.sh
@@ -3,12 +3,7 @@
 echo "Starting setup..."  
 
 python -m pip install --upgrade pip
-pip install tensorflow
-pip install onnxruntime
-pip install onnxruntime-gpu
-pip install opencv-python
-pip install cv2-enumerate-cameras
-pip install insightface
+pip install -r requirements.txt
 
 apt update
 apt install ffmpeg
