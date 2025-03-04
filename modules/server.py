568c14f79a850c7246affcde9a46189370843db1
everythinguwannatobe <adonishartsuper@gmail.com>
2025-03-04 17:39:24 +0200

    change requirement.txt

--- modules/server.py
+++ modules/server.py
@@ -19,7 +19,7 @@ from modules.face_analyser import (
 
 # Server configuration  
 HOST = '0.0.0.0'  # Replace with your server's IP address  
-PORT = 21759
+PORT = 31259
 
 def handle_client(conn, addr):  
     print(f"Connection from: {addr}")  
